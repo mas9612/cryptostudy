e043e7ef81ecbb286aa07a40427e5d530745e496
Naoki Harima <k905672@kansai-u.ac.jp>
2018-11-05 17:03:27 +0900

    add: pow test

--- aes/util_test.go
+++ aes/util_test.go
@@ -29,3 +29,48 @@ func TestMul(t *testing.T) {
 		}
 	}
 }
+
+func TestXtime(t *testing.T) {
+	inputs := []byte{
+		0x01,
+		0x02,
+		0x04,
+		0x08,
+		0x10,
+		0x20,
+		0x40,
+		0x80,
+		0x1b,
+		0x36,
+		0x6c,
+		0xd8,
+		0xab,
+		0x4d,
+		0x9a,
+	}
+
+	expected := []byte{
+		0x02,
+		0x04,
+		0x08,
+		0x10,
+		0x20,
+		0x40,
+		0x80,
+		0x1b,
+		0x36,
+		0x6c,
+		0xd8,
+		0xab,
+		0x4d,
+		0x9a,
+		0x2f,
+	}
+
+	for i, input := range inputs {
+		result := xtime(input)
+		if result != expected[i] {
+			t.Errorf("[TestXtime] case %d failed: result != expected : '%v' != '%v'", i, result, expected[i])
+		}
+	}
+}
