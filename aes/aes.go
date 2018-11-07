05cafd7998ed2a33a7617bde188b95797eb9a1a8
Naoki Harima <k905672@kansai-u.ac.jp>
2018-11-07 12:52:07 +0900

    add: cbc with cts mode. invCbcCtsCipher is not working with correct answer

--- aes/aes.go
+++ aes/aes.go
@@ -44,6 +44,8 @@ func Cipher(in []byte, key []byte, mode int, iv []byte) []byte {
 		out = ofbCipher(in, expandedKey, iv, numOfBlocks)
 	case ModeCTR:
 		out = ctrCipher(in, expandedKey, iv, numOfBlocks)
+	case ModeCBC_CTS:
+		out = cbcCtsCipher(in, expandedKey, iv, numOfBlocks)
 	default:
 		log.Fatalln("Invalid encryption mode")
 	}
@@ -89,6 +91,8 @@ func InvCipher(in, key []byte, mode int, iv []byte) []byte {
 		out = ofbInvCipher(in, expandedKey, iv, numOfBlocks)
 	case ModeCTR:
 		out = ctrInvCipher(in, expandedKey, iv, numOfBlocks)
+	case ModeCBC_CTS:
+		out = cbcCtsInvCipher(in, expandedKey, iv, numOfBlocks)
 	default:
 		log.Fatalln("Invalid encryption mode")
 	}
--- aes/mode.go
+++ aes/mode.go
@@ -145,6 +145,96 @@ func cbcInvCipher(in, key, iv []byte, numOfBlocks int) []byte {
 	return out[:end]
 }
 
+func cbcCtsCipher(in, key, iv []byte, numOfBlocks int) []byte {
+	if len(iv) != Nb*BytesOfWords {
+		log.Fatalf("IV must be same as block size (%d byte)", Nb*BytesOfWords)
+	}
+
+	out := make([]byte, len(in))
+	previous := make([]byte, Nb*BytesOfWords)
+	copy(previous, iv)
+
+	for i := 0; i < numOfBlocks; i++ {
+		state := make([]byte, Nb*BytesOfWords)
+		from := i * Nb * BytesOfWords
+		to := (i + 1) * Nb * BytesOfWords
+
+		stateLength := to - from
+		if i == numOfBlocks-1 {
+			stateLength = len(in) - from
+			// add padding with previous tail bytes
+			copy(state[stateLength:], previous[stateLength:])
+		}
+		copy(state[:stateLength], in[from:from+stateLength])
+
+		// XOR with previous cipher block
+		for j := 0; j < stateLength; j++ {
+			state[j] ^= previous[j]
+		}
+
+		blockCipher(state, key)
+		if i == numOfBlocks-1 {
+			copy(out[from-Nb*BytesOfWords:from], state)
+			copy(out[from:from+stateLength], previous[:stateLength])
+		} else {
+			copy(out[from:from+stateLength], state)
+			// save cipher block for next block
+			copy(previous, state)
+		}
+	}
+	return out
+}
+
+func cbcCtsInvCipher(in, key, iv []byte, numOfBlocks int) []byte {
+	if len(iv) != Nb*BytesOfWords {
+		log.Fatalf("IV must be same as block size (%d byte)", Nb*BytesOfWords)
+	}
+
+	out := make([]byte, len(in))
+	previous := make([]byte, Nb*BytesOfWords)
+
+	copy(previous, iv)
+	stateLength := Nb * BytesOfWords
+	for i := 0; i < numOfBlocks-2; i++ {
+		state := make([]byte, Nb*BytesOfWords)
+		tmp := make([]byte, Nb*BytesOfWords)
+		from := i * Nb * BytesOfWords
+		copy(state, in[from:from+stateLength])
+		copy(tmp, state)
+		invCipher(state, key)
+		// XOR with previous cipher block
+		for j := 0; j < stateLength; j++ {
+			state[j] ^= previous[j]
+		}
+		copy(out[from:from+stateLength], state)
+		copy(previous, tmp)
+	}
+
+	// stateN is last cipher block & n-1th plain text block
+	stateN := make([]byte, Nb*BytesOfWords)
+	stateN_1 := make([]byte, Nb*BytesOfWords)
+	lastBlockLength := len(in) % (Nb * BytesOfWords)
+
+	copy(stateN_1, in[len(in)-lastBlockLength-stateLength:len(in)-lastBlockLength])
+	invCipher(stateN_1, key)
+	copy(stateN[:lastBlockLength], in[len(in)-lastBlockLength:])
+	copy(stateN[lastBlockLength:], stateN_1[lastBlockLength:])
+
+	// XOR with last cipher block with padding
+	for j := 0; j < lastBlockLength; j++ {
+		stateN_1[j] ^= stateN[j]
+	}
+	copy(out[len(in)-lastBlockLength:], stateN_1[:lastBlockLength])
+
+	// XOR with previous cipher block
+	for j := 0; j < stateLength; j++ {
+		stateN[j] ^= previous[j]
+	}
+	copy(out[len(in)-lastBlockLength-stateLength:len(in)-lastBlockLength], stateN)
+
+	return out[:len(in)]
+}
+
 func cfbCipher(in, key, iv []byte, numOfBlocks int) []byte {
 	out := make([]byte, numOfBlocks*Nb*BytesOfWords)
 	previous := make([]byte, Nb*BytesOfWords)
