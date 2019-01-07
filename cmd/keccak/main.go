279194e50e2b279bae861252634b540708ec744e
Naoki Harima <k905672@kansai-u.ac.jp>
2019-01-07 18:26:24 +0900

    add : Made only form

---
+++ cmd/keccak/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/mas9612/cryptostudy/pkg/keccak"
+)
+
+func main() {
+	len := flag.Int("d", 256, "The length of the digest of a hash function. Default is \"256\". Valid length is one of [224, 256, 384, 512]")
+	flag.Parse()
+
+	switch *len {
+	case 224, 256, 384, 512:
+	default:
+		*len = 256
+	}
+
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	hash, err := keccak.Keccak(d, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%x\n", hash)
+}
